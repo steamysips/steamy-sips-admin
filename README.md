f49182bb157baa2b6a4d275bdcbfd826d9275e43
creme332 <65414576+creme332@users.noreply.github.com>
2024-07-21 19:32:26 +0400

    initial setup

--- README.md
+++ README.md
@@ -1,70 +1,7 @@
-# MRN-template 🦄
+# steamy-sips-admin ☕
 ![Next JS](https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white)
 ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white)
 ![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
 ![Mantine](https://img.shields.io/badge/Mantine-16B7FB?style=for-the-badge&logo=mantine&logoColor=black)
 
-This is a [Next.js](https://nextjs.org/) project using React and the Mantine UI library.  
-
-![Screenshot of website](image.png)
-
-[Live Preview ▶]()
-
-## Features
-- Mantine (v7), including the forms management library and the dates package
-- React (v18)
-- Tabler icons (v2)
-## Installation
-Install the template:
-```bash
-git clone git@github.com:creme332/react-next-template.git
-```
-
-> 🔴 **Note**: Delete the `.git` folder in the root directory of this project if you are cloning this project inside another git folder.
-
-Install dependencies:
-```bash
-npm install
-```
-
-To run the development server:
-
-```bash
-npm run dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-To run production server:
-
-```bash
-npm run start
-```
-
-## Updating packages
-To check for outdated packages:
-```bash
-npm outdated
-```
-
-To update all dependencies, if you are confident this is desirable:
-```bash
-npm update --save
-```
-
-> 🟢 **Note**: To migrate packages to newer versions, always refer to the documentation.
-> 
-## To-do
-After installing the project, perform the following actions:
-- [ ] Update project title in `package.json` and `package-lock.json`
-- [ ] Ensure that you have the [necessary VSCode extensions](https://mantine.dev/getting-started/#set-up-vs-code)
-- [ ] Remove any unused dependencies
-- [ ] Update favicon in `_app.tsx`
-- [ ] Update website title and meta description in `_app.tsx`
-- [ ] Update font
-- [ ] Update `logo` in `_app.tsx`
-- [ ] Run lighthouse report
-- [ ] Use NextJS Link component instead of anchor tag
-
-## References
-- https://stackoverflow.com/a/16532884/17627866
+A content-management system for Steamy Sips.
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import "@mantine/core/styles.css";
 import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
+import {
+  MantineProvider,
+  Container,
+  AppShell,
+  Burger,
+  Group,
+  Skeleton,
+} from "@mantine/core";
 import { theme } from "../theme";
 import Header from "../components/HeaderSimple";
 import Footer from "../components/FooterSimple";
 import { IconBrandGooglePhotos } from "@tabler/icons-react";
+import { NavbarNested } from "../components/NavbarNested";
+import { useDisclosure } from "@mantine/hooks";
 
 interface AppProps {
   Component: () => JSX.Element;
@@ -12,21 +21,68 @@ interface AppProps {
 }
 
 export default function App({ Component, pageProps }: AppProps) {
+  const URL = "http://localhost:80/steamy-sips/public";
+  const [opened, { toggle }] = useDisclosure();
+
+  // (async function callAPI() {
+  //   try {
+  //     const response = await fetch(URL, {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     const json = await response.json();
+  //     console.log(json);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // })();
+
   return (
     <MantineProvider defaultColorScheme="dark" theme={theme}>
       <Head>
         <meta charSet="utf-8" />
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💧</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔒</text></svg>"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="description of website here" />
-        <title>Template</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <meta
+          name="description"
+          content="Admin website for the Steamy Sips e-commerce platform"
+        />
+        <meta name="robots" content="noindex, nofollow, noarchive" />
+        <title>Steamy Sips Admin</title>
       </Head>
-      <Header />
-      <Component {...pageProps} />
-      <Footer icon={<IconBrandGooglePhotos />} />
+      <AppShell
+        // header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+
+        {/* <AppShell.Header>
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        </AppShell.Header> */}
+        <AppShell.Navbar>
+          <NavbarNested />
+        </AppShell.Navbar>
+        <AppShell.Main>
+          <Component {...pageProps} />
+        </AppShell.Main>
+
+        {/* <AppShell.Footer>
+          <Footer></Footer>
+        </AppShell.Footer> */}
+      </AppShell>
     </MantineProvider>
   );
 }
