3fa447e4f6481b4811ee4d75d86f4f2a2662fab5
creme332 <65414576+creme332@users.noreply.github.com>
2024-08-31 15:41:02 +0400

    create login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,14 @@ import "@mantine/core/styles.css";
 import Head from "next/head";
 import {
   MantineProvider,
-  Container,
   AppShell,
   Burger,
-  Group,
-  Skeleton,
 } from "@mantine/core";
 import { theme } from "../theme";
-import Header from "../components/HeaderSimple";
-import Footer from "../components/FooterSimple";
-import { IconBrandGooglePhotos } from "@tabler/icons-react";
 import { NavbarNested } from "../components/NavbarNested";
 import { useDisclosure } from "@mantine/hooks";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 interface AppProps {
   Component: () => JSX.Element;
@@ -21,43 +17,18 @@ interface AppProps {
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-  const URL = "http://localhost:80/steamy-sips/public";
   const [opened, { toggle }] = useDisclosure();
+  const [loggedIn, setLoggedIn] = useState(false);
+  const router = useRouter();
 
-  // (async function callAPI() {
-  //   try {
-  //     const response = await fetch(URL, {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     });
-  //     const json = await response.json();
-  //     console.log(json);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // })();
+  useEffect(() => {
+    if (!loggedIn) {
+      router.push("/");
+    }
+  }, [loggedIn, router]);
 
-  return (
-    <MantineProvider defaultColorScheme="dark" theme={theme}>
-      <Head>
-        <meta charSet="utf-8" />
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔒</text></svg>"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
-        <meta
-          name="description"
-          content="Admin website for the Steamy Sips e-commerce platform"
-        />
-        <meta name="robots" content="noindex, nofollow, noarchive" />
-        <title>Steamy Sips Admin</title>
-      </Head>
+  function getMainLayout() {
+    return (
       <AppShell
         // header={{ height: 60 }}
         navbar={{
@@ -68,21 +39,37 @@ export default function App({ Component, pageProps }: AppProps) {
         padding="md"
       >
         <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-
-        {/* <AppShell.Header>
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-        </AppShell.Header> */}
         <AppShell.Navbar>
           <NavbarNested />
         </AppShell.Navbar>
+
         <AppShell.Main>
           <Component {...pageProps} />
         </AppShell.Main>
-
-        {/* <AppShell.Footer>
-          <Footer></Footer>
-        </AppShell.Footer> */}
       </AppShell>
+    );
+  }
+  return (
+    <MantineProvider theme={theme}>
+      <Head>
+        <meta charSet="utf-8" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔒</text></svg>"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Admin website for the Steamy Sips e-commerce platform"
+        />
+        <meta name="robots" content="noindex, nofollow, noarchive" />
+        <title>Steamy Sips Admin</title>
+      </Head>
+      {router.pathname == "/" ? (
+        <Component {...pageProps} />
+      ) : (
+        getMainLayout()
+      )}
     </MantineProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,44 @@
-import { Button, Group } from "@mantine/core";
+import { Paper, TextInput, PasswordInput, Button, Title } from "@mantine/core";
+import classes from "../styles/AuthenticationImage.module.css";
 
-export default function IndexPage() {
+export default function LoginPage() {
+  async function callAPI() {
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL ?? "", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <Group mt={50} justify="center">
-      <Button size="xl">Welcome to Mantine!</Button>
-    </Group>
+    <div className={classes.wrapper}>
+      <Paper className={classes.form} radius={0} p={30}>
+        <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
+          Steamy Sips Admin Portal
+        </Title>
+
+        <TextInput
+          label="Email address"
+          placeholder="admin@steamy.com"
+          size="md"
+        />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          mt="md"
+          size="md"
+        />
+        <Button fullWidth mt="xl" size="md">
+          Login
+        </Button>
+      </Paper>
+    </div>
   );
 }
